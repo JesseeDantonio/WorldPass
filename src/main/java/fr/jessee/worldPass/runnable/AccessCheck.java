f9f47f247243433e9b9d7537dbd35ed852b0f3ac
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-02-26 10:20:11 +0100

    - Implémentation des restrictions d'accès en fonction :   - Du temps de jeu minimum requis pour certains mondes.   - Des plages horaires autorisées pour un accès spécifique. - Ajout de messages pour informer les joueurs expulsés des raisons. - Ajout de journaux pour faciliter le débogage et le suivi.

--- src/main/java/fr/jessee/worldPass/runnable/AccessCheck.java
+++ src/main/java/fr/jessee/worldPass/runnable/AccessCheck.java
@@ -3,6 +3,7 @@ package fr.jessee.worldPass.runnable;
 import fr.jessee.worldPass.RestrictedWorld;
 import fr.jessee.worldPass.WorldPass;
 import org.bukkit.ChatColor;
+import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -19,13 +20,27 @@ public class AccessCheck extends BukkitRunnable {
     @Override
     public void run() {
         for (Map.Entry<UUID, RestrictedWorld> entry : worldPass.getRestrictedWorlds().entrySet()) {
-            if (!worldPass.getTimeValidator().isWithinTimeRange(entry.getValue().startTime(), entry.getValue().endTime())) {
+            worldPass.getLogger().info("Checking.. " + entry.getKey() + " " +entry.getValue().world().getName());
+            if (!(worldPass.getTimeValidator().isWithinTimeRange(entry.getValue().startTime(), entry.getValue().endTime()))) {
                 for (Player player : entry.getValue().world().getPlayers()) {
-                    String startFormatted = String.format("%02d:%02d", entry.getValue().startTime().getHour(), entry.getValue().startTime().getMinute());
-                    String endFormatted = String.format("%02d:%02d", entry.getValue().endTime().getHour(), entry.getValue().endTime().getMinute());
+                    if (entry.getValue().restrictOnlyNewPlayers()) {
+                        // Récupère le temps de jeu du joueur
+                        int playTimeTicks = player.getStatistic(Statistic.PLAY_ONE_MINUTE);
+                        int playTimeMinutes = playTimeTicks / (20 * 60);
 
-                    player.kickPlayer(ChatColor.RED + "Vous ne pouvez pas être dans le monde " + entry.getValue().world().getName() +
-                            " en dehors des heures autorisées (" + startFormatted + " - " + endFormatted + ").");
+                        worldPass.getLogger().info("PlayTimeMinutes " + playTimeMinutes + " " + player.getName());
+                        worldPass.getLogger().info("PlayTimeMinutes " + playTimeMinutes + " < " + "minimumPlayTimeMinutes " + entry.getValue().minimumPlayTimeMinutes());
+                        // Vérifie si le joueur a atteint le temps de jeu minimum
+                        if (playTimeMinutes < entry.getValue().minimumPlayTimeMinutes()) {
+                            player.kickPlayer("Vous ne pouvez pas entrer dans ce monde car vous êtes un joueur inexpérimenté !");
+                        }
+                    } else {
+                        String startFormatted = String.format("%02d:%02d", entry.getValue().startTime().getHour(), entry.getValue().startTime().getMinute());
+                        String endFormatted = String.format("%02d:%02d", entry.getValue().endTime().getHour(), entry.getValue().endTime().getMinute());
+
+                        player.kickPlayer(ChatColor.RED + "Vous ne pouvez pas être dans le monde " + entry.getValue().world().getName() +
+                                " en dehors des heures autorisées (" + startFormatted + " - " + endFormatted + ").");
+                    }
                 }
             }
         }
