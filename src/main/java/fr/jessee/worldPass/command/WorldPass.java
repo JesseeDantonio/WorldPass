ababb4af4d3dcaa81114285574dac80c58e48d4b
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-02-27 12:42:41 +0100

    - Modification de la structure pour detecter des arguments - Implémentation d'un message à la fin de la procedure
    Cause :

--- src/main/java/fr/jessee/worldPass/command/WorldPass.java
+++ src/main/java/fr/jessee/worldPass/command/WorldPass.java
@@ -15,22 +15,37 @@ public class WorldPass implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (command.getName().equalsIgnoreCase("worldpass")) {
-            if (commandSender instanceof Player player) {
-                player.sendMessage(
-                        ChatColor.AQUA +
-                                "Mise à jour des informations.."
-                );
-            } else {
-                fr.jessee.worldPass.WorldPass.getInstance().getLogger().warning("Mise à jour des informations..");
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (command.getName().equalsIgnoreCase("worldpass") && args.length > 0) {
+            switch (args[0].toLowerCase()) {
+                case "update", "reload": {
+                    if (commandSender instanceof Player player) {
+                        player.sendMessage(
+                                ChatColor.AQUA +
+                                        "Mise à jour des informations.."
+                        );
+                    } else {
+                        fr.jessee.worldPass.WorldPass.getInstance().getLogger().warning("Mise à jour des informations..");
+                    }
+                    if (worldPass.getAccessCheck() != null && worldPass.getAccessCheck().isCancelled()) {
+                        worldPass.getAccessCheck().cancel();
+                    }
+                    worldPass.updateConfig();
+                    worldPass.setAccessCheck(new AccessCheck(worldPass));
+                    worldPass.getAccessCheck().runTaskTimer(worldPass, 0L, 20L * 60);
+                    if (commandSender instanceof Player player) {
+                        player.sendMessage(
+                                ChatColor.GREEN +
+                                        "Mise à jour terminée"
+                        );
+                    } else {
+                        fr.jessee.worldPass.WorldPass.getInstance().getLogger().warning("Mise à jour terminée");
+                    }
+                }
+                default: {
+                    return false;
+                }
             }
-            if (worldPass.getAccessCheck() != null && worldPass.getAccessCheck().isCancelled()) {
-                worldPass.getAccessCheck().cancel();
-            }
-            worldPass.updateConfig();
-            worldPass.setAccessCheck(new AccessCheck(worldPass));
-            worldPass.getAccessCheck().runTaskTimer(worldPass, 0L, 20L * 60);
         }
         return true;
     }
