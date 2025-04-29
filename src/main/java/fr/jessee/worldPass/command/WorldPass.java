36ef5bd2abc99dedb64d328e926c9d791d8e6145
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-04-29 12:49:50 +0200

    Refactorisation pour accueillir d'autres commandes.
    Cause : Refactorisation

--- src/main/java/fr/jessee/worldPass/command/WorldPass.java
+++ src/main/java/fr/jessee/worldPass/command/WorldPass.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import static org.bukkit.Bukkit.getConsoleSender;
+
 public class WorldPass implements CommandExecutor {
     private final fr.jessee.worldPass.WorldPass worldPass;
 
@@ -17,35 +19,50 @@ public class WorldPass implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if (command.getName().equalsIgnoreCase("worldpass") && args.length > 0) {
-            switch (args[0].toLowerCase()) {
-                case "update", "reload": {
+            return switch (args[0].toLowerCase()) {
+                case "update", "reload", "restart" -> this.handleReload(commandSender);
+                default -> {
+                    String message = "Commande inconnue.";
                     if (commandSender instanceof Player player) {
-                        player.sendMessage(
-                                ChatColor.AQUA +
-                                        "Mise à jour des informations.."
-                        );
+                        player.sendMessage(ChatColor.RED + message);
                     } else {
-                        fr.jessee.worldPass.WorldPass.getInstance().getLogger().warning("Mise à jour des informations..");
+                        getConsoleSender().sendMessage(message);
                     }
-                    if (worldPass.getAccessCheck() != null && worldPass.getAccessCheck().isCancelled()) {
-                        worldPass.getAccessCheck().cancel();
-                    }
-                    worldPass.updateConfig();
-                    worldPass.setAccessCheck(new AccessCheck(worldPass));
-                    worldPass.getAccessCheck().runTaskTimer(worldPass, 0L, 20L * 60);
-                    if (commandSender instanceof Player player) {
-                        player.sendMessage(
-                                ChatColor.GREEN +
-                                        "Mise à jour terminée"
-                        );
-                    } else {
-                        fr.jessee.worldPass.WorldPass.getInstance().getLogger().warning("Mise à jour terminée");
-                    }
+                    yield true;
                 }
-                default: {
-                    return false;
-                }
+            };
+        }
+        return true;
+    }
+
+    private boolean handleReload(CommandSender commandSender) {
+        if (commandSender instanceof Player player) {
+            if (!(player.hasPermission("worldpass.reload"))) {
+                player.sendMessage(
+                        ChatColor.RED +
+                                "Vous n'avez pas la permission !"
+                );
             }
+            player.sendMessage(
+                    ChatColor.YELLOW +
+                            "Mise à jour des informations en cours."
+            );
+        } else {
+            fr.jessee.worldPass.WorldPass.getInstance().getLogger().warning("Mise à jour des informations en cours.");
+        }
+        if (worldPass.getAccessCheck() != null && worldPass.getAccessCheck().isCancelled()) {
+            worldPass.getAccessCheck().cancel();
+        }
+        worldPass.updateConfig();
+        worldPass.setAccessCheck(new AccessCheck(worldPass));
+        worldPass.getAccessCheck().runTaskTimer(worldPass, 0L, 20L * 60);
+        if (commandSender instanceof Player player) {
+            player.sendMessage(
+                    ChatColor.GREEN +
+                            "Mise à jour des informations terminée."
+            );
+        } else {
+            fr.jessee.worldPass.WorldPass.getInstance().getLogger().warning("Mise à jour des informations terminée.");
         }
         return true;
     }
