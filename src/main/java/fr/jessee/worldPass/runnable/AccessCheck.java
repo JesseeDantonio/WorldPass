aa78d250c068700104e953073d6e7a512214664c
JesseeDantonio <dyland.antonio.dev42@gmail.com>
2025-04-29 10:26:06 +0200

    Le temps n'est plus globale, il est propre à chaque monde désormais.
    Cause :

--- src/main/java/fr/jessee/worldPass/runnable/AccessCheck.java
+++ src/main/java/fr/jessee/worldPass/runnable/AccessCheck.java
@@ -2,11 +2,10 @@ package fr.jessee.worldPass.runnable;
 
 import fr.jessee.worldPass.RestrictedWorld;
 import fr.jessee.worldPass.WorldPass;
-import org.bukkit.ChatColor;
-import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.UUID;
 
@@ -20,13 +19,20 @@ public class AccessCheck extends BukkitRunnable {
     @Override
     public void run() {
         for (Map.Entry<UUID, RestrictedWorld> entry : worldPass.getRestrictedWorlds().entrySet()) {
-            //worldPass.getLogger().info("Checking.. " + entry.getKey() + " " +entry.getValue().world().getName());
+            // worldPass.getLogger().info("Checking.. " + entry.getKey() + " " +entry.getValue().world().getName());
             if (!(worldPass.getTimeValidator().isWithinTimeRange(entry.getValue().startTime(), entry.getValue().endTime()))) {
-                for (Player player : entry.getValue().world().getPlayers()) {
+                for (Player player : entry.getValue().world().getPlayers().stream().filter(
+                        player -> player.getWorld().getUID().equals(entry.getKey())
+                ).toList()) {
                     if (entry.getValue().restrictOnlyNewPlayers()) {
                         // Récupère le temps de jeu du joueur
-                        int playTimeTicks = player.getStatistic(Statistic.PLAY_ONE_MINUTE);
-                        int playTimeMinutes = playTimeTicks / (20 * 60);
+                        long playTimeTicks = 0;
+                        try {
+                            playTimeTicks = worldPass.getPlayTime().p(player.getUniqueId(), entry.getKey());
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                        long playTimeMinutes = playTimeTicks / (20 * 60);
 
                         //worldPass.getLogger().info("PlayTimeMinutes " + playTimeMinutes + " " + player.getName());
                         //worldPass.getLogger().info("PlayTimeMinutes " + playTimeMinutes + " < " + "minimumPlayTimeMinutes " + entry.getValue().minimumPlayTimeMinutes());
@@ -44,5 +50,17 @@ public class AccessCheck extends BukkitRunnable {
                 }
             }
         }
+    }
+
+    private static Map<String, String> getPlaceholders(Map.Entry<UUID, RestrictedWorld> entry) {
+        String startFormatted = String.format("%02d:%02d", entry.getValue().startTime().getHour(), entry.getValue().startTime().getMinute());
+        String endFormatted = String.format("%02d:%02d", entry.getValue().endTime().getHour(), entry.getValue().endTime().getMinute());
+
+        Map<String, String> placeholders = Map.of(
+                "world", entry.getValue().world().getName(),
+                "start", startFormatted,
+                "end", endFormatted
+        );
+        return placeholders;
     }
 }
